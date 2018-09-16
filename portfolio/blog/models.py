efc2084a3290a71cd118afb2ef2665193e1375ae
jeff <ahuimanu@gmail.com>
2018-09-15 23:05:34 -0500

    updated blog entity

--- portfolio/blog/models.py
+++ portfolio/blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+# create blog model
+# add blog app to settings
+# create migration
+# run migration
+# add to admin
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField(auto_now=True)
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
